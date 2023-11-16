dd18d16562deb70e900e41e0eb0e02d318275eb6
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-16 10:42:25 +0600

    checking css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,16 @@
 <script>
-  import "../app.postcss";
+    import "../app.postcss";
+    import "../app.css";
+    import Footer from "../lib/components/ui/Footer.svelte";
+    import Navbar from "../lib/components/ui/Navbar.svelte";
 </script>
-
-<slot />
+<div class=" relative w-full text-white overflow-hidden min-h-screen">
+  <Navbar/>
+    <slot />
+  <Footer/>
+  <div class="absolute shade-1 bg-teal-400/50 left-1 opacity-40 " />
+  <div class='absolute gradient-01 opacity-50 left-1 ' />
+  <div class='absolute gradient-02 opacity-50 bottom-4 left-1' />
+  <div class='absolute gradient-03 opacity-50 right-1 top-[50%] ' />
+  <div class='absolute gradient-04 opacity-30 top-10 right-[50%] ' />
+</div>
