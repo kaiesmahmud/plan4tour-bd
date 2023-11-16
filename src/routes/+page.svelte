5cd42ec327326d5783d783927ceb6a55bb5d913c
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-16 14:27:24 +0600

    updated

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script>
-	import FAQ from './../lib/components/ui/FAQ.svelte';
+	import FAQ from '../lib/components/FAQ.svelte';
+    import HeroSection from '../lib/components/HeroSection.svelte';
 
 </script>
-<h1 class="text-4xl text-center">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
+<HeroSection/>
+<h1 class="text-4xl text-center ">Welcome to SvelteKit</h1>
 <FAQ />
