012e4b94e3bd2e1e38a1eb31a4140cd51691e941
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-16 14:26:10 +0600

    Hero created and updating

---
+++ src/lib/components/HeroSection.svelte
@@ -0,0 +1,10 @@
+<script>
+
+</script>
+<div class="min-h-screen" >
+    <div class=" w-full h-[50vh] md:h-[70vh] overflow-hidden flex items-center justify-center">
+        <img  src="https://images.unsplash.com/photo-1465778893808-9b3d1b443be4?q=80&w=2075&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+    </div>
+    <h1 class=" text-center">Hero SEction Here</h1>
+</div>
+
