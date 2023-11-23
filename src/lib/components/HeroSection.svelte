613171f7d347849e9d18f68bc8fbbc55086eafb7
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-23 17:29:41 +0600

    banner img add

--- src/lib/components/HeroSection.svelte
+++ src/lib/components/HeroSection.svelte
@@ -16,8 +16,8 @@
     // ]
 </script>
 <div class="min-h-screen" >
-    <div class=" w-full  md:h-[70vh] overflow-hidden flex items-center justify-center">
-        <img  src="https://images.unsplash.com/photo-1465778893808-9b3d1b443be4?q=80&w=2075&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+    <div class=" w-full   overflow-hidden flex items-center justify-center">
+        <img  src="/plan4tour-1.jpeg" class="w-full" alt="">
     </div>
     <h1 class=" text-center">Hero SEction Here</h1>
     <p class=" text-center">Will Be Updating</p>
