665dd83f32b48bff306eddd577f9a7def5a7c1ed
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-24 20:54:12 +0600

    Testing Carousel Component created

---
+++ src/lib/components/TestCarousel.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { Navigation, Pagination, A11y } from 'swiper';
+    import { Swiper, SwiperSlide } from 'swiper';
+  
+      let swiper;
+  </script>
+  
+  <svelte:head>
+      <link rel="stylesheet" href="https://unpkg.com/swiper@8.2.4/swiper-bundle.css"/>
+  </svelte:head>
+  
+  <Swiper modules={[Navigation, Pagination, A11y]}
+                  slidesPerView={1} navigation
+                  on:swiper={e => swiper = e.detail[0]}>
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+  </Swiper>
+  
+  <button class="p-5 bg-green-500" type=button on:click={() => swiper.slidePrev()}>Previous</button>
+  <button class="p-5 bg-green-500" type=button on:click={() => swiper.slideNext()}>Next</button>
