56a78f6f48933fba90ea35cf36445a19a0e45ae9
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-24 20:54:42 +0600

    testin swiper js

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,16 @@
 <script>
+    import 'swiper/css';
+    import'swiper/css/navigation';
+    import'swiper/css/pagination';
+    import'swiper/css/scrollbar';
+    import'swiper/css/effect-fade';
+
 	import FAQ from '../lib/components/FAQ.svelte';
     import HeroSection from '../lib/components/HeroSection.svelte';
-
+    // import TestCarousel from '../lib/components/TestCarousel.svelte';
+    // import Carousel from 'svelte-carousel'
 </script>
+<!-- <TestCarousel/> -->
 <HeroSection/>
 <h1 class="text-4xl text-center p-2">Welcome to SvelteKit</h1>
 <FAQ />
