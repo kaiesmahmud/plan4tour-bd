489b2536a6418d00ced92e17e425fa3b401db947
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-26 12:45:23 +0600

    services section added

--- src/lib/components/HeroSection.svelte
+++ src/lib/components/HeroSection.svelte
@@ -1,21 +1,38 @@
 <script>
-    import Carousel from 'svelte-carousel'
-    let demos = [0,0,0,0]
-    let imgSlides = [
-        {
-            url: "https://images.unsplash.com/photo-1465778893808-9b3d1b443be4?q=80&w=2075&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        },
-        {
-            url: "https://images.unsplash.com/photo-1615277716590-d87d101caa70?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        },
-        {
-            url: "https://images.unsplash.com/photo-1482332486572-10fff1bedfbe?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        },
-        
-        
-        
-    ]
+        import Icon from '@iconify/svelte';
+
     // import {Swiper,SwiperSlide} from 'swiper';
+    const Services = [
+        {
+            id: '01',
+            title: "Hotels",
+            icon: "carbon:hotel",
+            url: "/hotels",
+            description: "Compare hotel prices and find the best deals"
+
+        },
+        {
+            id: '02',
+            title: "Tour Places",
+            icon: "mdi:place-outline",
+            url: "/touring-places",
+            description: "Let us guide you through the wonders of the world"
+        },
+        {
+            id: '03',
+            title: "Tour Blogs",
+            icon: "carbon:blog",
+            url: "/tour-blogs",
+            description: "Let our travel tales ignite your passion for adventure"
+        },
+        {
+            id: '04',
+            title: "Travel Tickets",
+            icon: "ion:ticket-outline",
+            url: "/travel-tickets",
+            description: "Book your round-trip flights and save up to 50% with our special offers"
+        },
+    ]
 </script>
 
 <svelte:head>
@@ -26,33 +43,32 @@
     <div class=" w-full    overflow-hidden flex items-center justify-center">
         <img  src="/plan4tour-1.jpeg" class="w-full md:w-[80%] rounded" alt="">
     </div>
-    <h1 class=" text-center">Hero SEction Here</h1>
-    <p class=" text-center">Will Be Updating</p>
-    <div class="p-5">
-        <!-- <Swiper>
-            <SwiperSlide>a</SwiperSlide>
-            <SwiperSlide>b </SwiperSlide>
-            <SwiperSlide>c</SwiperSlide>
-            <SwiperSlide>d</SwiperSlide>
-        </Swiper> -->
-        <!-- <Carousel
-        autoplay
-        autoplayDuration={2000}
-        dots={false}
-        arrows={false}
-        >
-            <div class="w-full p-5">
-                <img  src="https://images.unsplash.com/photo-1503220317375-aaad61436b1b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="w-full" alt="">
-            </div>
-            <div class="w-full p-5">
-                <img  src="https://images.unsplash.com/photo-1503220317375-aaad61436b1b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="w-full" alt="">
-            </div>
-            <div class="w-full p-5">
-                <img  src="https://images.unsplash.com/photo-1503220317375-aaad61436b1b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="w-full" alt="">
-            </div>
-        </Carousel> -->
+    <div>
 
     </div>
+    <div class="w-full flex items-center justify-center mt-5 md:mt-10">
+        <!-- <div class="p-5 w-full md:w-[70%] md:bg-black/90 flex items-center justify-center gap-3 rounded flex-col md:flex-row"> -->
+        <div class="p-5 w-full md:w-[70%]  rounded grid grid-cols-2 md:grid-cols-4 gap-2 text-center">
+            {#each Services as service(service.id)}
+                <div class="bg-white/10 md:bg-white/20 rounded p-2 md:p-5 pt-4 flex flex-col items-center justify-between gap-3 md:gap-5">
+                    <div>
+                        <div class="text-center flex items-center flex-col gap-1">
+                            <div class="text-3xl lg:text-5xl">
+                                <Icon icon={service.icon} />
+                            </div>
+                            <p>{service.title}</p>
+                        </div>
+                        <p class="text-xs md:text-sm text-center text-white/50">{service.description}</p>
+                    </div>
+                    <a href={service.url} class="w-full bg-black/50 font-semibold rounded text-sm p-3 "> View Details</a>
+                </div>
+            {/each}
+    
+        </div>
+
+    </div>
+    <h1 class=" text-center">Hero SEction Here</h1>
+    <p class=" text-center">Will Be Updating</p>
 
 </div>
 
