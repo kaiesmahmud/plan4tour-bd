31642b48fb2334558db354de61fdf48c7dbabb71
kaiesmahmud <kaiesmahmud@gmail.com>
2023-11-26 12:04:06 +0600

    meta title,description added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,10 @@
     // import TestCarousel from '../lib/components/TestCarousel.svelte';
     // import Carousel from 'svelte-carousel'
 </script>
+<svelte:head>
+    <title>Plan4Tour </title> 
+	<meta name="description" content="Experience effortless page name editing with the Drag-N-Drop Page Name Editor in SvelteKit by Kaies Mahmud Nehal. Seamlessly designed using Tailwind CSS and Svelte-DND-Action, enriched with Iconify icons. Elevate your web development with intuitive drag-and-drop functionality. Unlock user-friendly design and efficiency in one powerful tool" />
+</svelte:head>
 <!-- <TestCarousel/> -->
 <HeroSection/>
 <h1 class="text-4xl text-center p-2">Welcome to SvelteKit</h1>
